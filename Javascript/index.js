a99c1a16a74c82af82f1ea431ba9ca31f0746806
Nikesh Rajbhar <nikesh21soni@gmail.com>
2024-10-04 20:43:34 +0530

    handle Outgoing message

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,38 @@
 const typingForm = document.querySelector(".typing-form");
+const chatList = document.querySelector(".chat-list");
 
-typingForm.addEventListener("submit" , (e) => {
+let userMessage = null;
+
+//  create a new message element and return it..
+
+const createMessageElement = (content, className) => {
+    const divTag = document.createElement("div");
+    divTag.classList.add("message", className);
+    divTag.innerHTML = content;
+
+    return divTag
+}
+
+const handleOutgoingChat = () => {
+    userMessage = typingForm.querySelector(".typing-input").value.trim()
+
+    if (!userMessage) return // exit if no message 
+
+    const htmlCode = `
+            <div class="message-content d-flex gap-3 mt-3">
+                <img src="./images/user.jpg" alt="User Image" class="avatar">
+                <p class="text p-0 m-0"> </p>
+            </div>
+    `;
+
+   const outGoingMessageDiv = createMessageElement(htmlCode, "outgoing");
+   outGoingMessageDiv.querySelector(".text").innerText = userMessage
+   chatList.appendChild(outGoingMessageDiv);
+}
+
+typingForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    
+
+    handleOutgoingChat();
 
 })
